1b5d1d9cd7cea3d03bb83776860c9c8a41ff0efe
Kevin <8477253+made-by-kevin@users.noreply.github.com>
2025-07-02 14:28:35 +0200

    Prepared for PDF generation

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -2,7 +2,8 @@ export FORCE_SHOW_EDIT_PAGE_LINK=true
 npx antora antora-playbook.yml
 
 target='./build'
-sed -i -e 's/noindex/all/g' $target/index.html
+# sed -i -e 's/noindex/all/g' $target/index.html
+perl -i -pe's/noindex/all/g' $target/index.html
 cp ./supplemental/googleeb9c46405b5f49a5.html $target/googleeb9c46405b5f49a5.html
 rm -r $target/ui/partials
 rm -f $target/ui/css/*.map
--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -1,6 +1,18 @@
 target='./build'
+rel='./miterra-site'
+
+rm -f $target/.nojekyll
 rm -f $target/googleeb9c46405b5f49a5.html
 rm -f $target/404.html
 rm -f $target/robots.txt
 rm -f $target/sitemap*.xml
-mv $target ./miterra-site
+
+brew install --cask prince
+
+html=$target/miterra-docs/main/print.html
+prince --javascript $html -o ./miterra-docs.pdf
+
+rm -f $html
+
+mv $target $rel
+zip -qq -r miterra-site.zip $rel
